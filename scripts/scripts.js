f6b6e6bcb9b1fc7b47f5b2ddb4b311f49a16eb8f
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-13 00:00:57 +0300

    Edited number validation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,5 @@
 // Declare the input fields
 const fullName = document.getElementById('name');
-const number = document.getElementById('number');
 const message = document.getElementById('message');
 // Declare send button
 const sendButton = document.getElementById('send');
@@ -17,6 +16,10 @@ function validate() {
     // Declare limitation for email
     let email = document.getElementById('email');
     let emailFormat=/^[\w.-]{3,}@\w{5,}.*$/;
+
+    // Declare limitation for number
+    let number = document.getElementById('number');
+    let numberFormat=/^\+9613[0-9]{0,6}|\+9617[0-9]{0,7}/;
 
     // validate full name
     if (fullName.value.length<=5) {
@@ -38,7 +41,16 @@ function validate() {
         document.querySelector('.error-box2').textContent = '-Wrong email format';
         document.getElementById('email').style.borderColor = '#ced4da';
     }
-
+    // validate number
+    if (numberFormat.test(number)) {
+        document.getElementById('number').style.borderColor = 'green';
+        document.querySelector('.error-box3').textContent = '';
+    }
+    else {
+        document.querySelector('.error-box3').classList.add('error-message'); 
+        document.querySelector('.error-box3').textContent = '-Wrong number format';
+        document.getElementById('number').style.borderColor = '#ced4da';
+    }
     // validate message
     if (message.value.length<=100) {
         document.querySelector('.error-box4').classList.add('error-message');
